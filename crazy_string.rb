aa80cb2e3b7baee9970aab498cd1e1005b794174
PythonKittyGirl <ung.deborah@gmail.com>
2018-08-07 20:57:42 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,6 +1,6 @@
 def crazy_strings(a,b)
 return a=a.reverse.upcase
-return b=b.gsub("S","Z").gsub("s","z").swapcase
+return b=b.gsub.swapcase("S","Z").gsub("s","z")
 
 end
 crazy_strings("apple", "Snake")
